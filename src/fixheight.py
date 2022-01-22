1a2ad070e903c21ac4da2d726bfd8809744c3de2
Hiroshi Miura <hirmiura@gmail.com>
2022-01-22 09:16:13 +0900

    feat: fntファイル2行目のlineHeightとbaseを変更機能を追加した

--- src/fixheight.py
+++ src/fixheight.py
@@ -14,8 +14,10 @@ def pargs() -> None:
     global args
     parser = argparse.ArgumentParser(
         description='fntファイルの高さのパラメータを変更する')
-    parser.add_argument('-y', type=int, default=0, help='yの変更値')
-    parser.add_argument('-t', type=int, default=0, help='heightの変更値')
+    parser.add_argument('-lh', type=int, default=None, help='lineHeightの値 - 元フォントに合わせると良い')
+    parser.add_argument('-ba', type=int, default=None, help='baseの変更値 - 元フォントに合わせると良い')
+    parser.add_argument('-y', type=int, default=0, help='yの変更値 - オフセット指定')
+    parser.add_argument('-t', type=int, default=0, help='heightの変更値 - オフセット指定')
     parser.add_argument('files', nargs='+', help='1つ以上の入力ファイル')
     parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
     args = parser.parse_args()
@@ -26,6 +28,11 @@ def process():
         fp = Path(file)
         text = fp.read_text(encoding='utf-8')
         lines = text.splitlines()
+        # 2行目を処理する
+        if args.lh:
+            lines[1] = re.sub(r' lineHeight=(\d+) ', f' lineHeight={args.lh} ')
+        if args.ba:
+            lines[1] = re.sub(r' base=(\d+) ', f' base={args.ba} ')
         # 3行目から処理する
         i: int = 2
         iMax: int = len(lines)
